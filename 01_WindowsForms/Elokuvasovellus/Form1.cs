d62f6ac347ddb69f9c940936af52e920706b4003
Antti Seppänen <antti.se@gmail.com>
2024-03-06 13:29:17 +0200

    Elokuvasovellus

---
+++ 01_WindowsForms/Elokuvasovellus/Form1.cs
@@ -0,0 +1,15 @@
+namespace Elokuvasovellus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTallenna_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
