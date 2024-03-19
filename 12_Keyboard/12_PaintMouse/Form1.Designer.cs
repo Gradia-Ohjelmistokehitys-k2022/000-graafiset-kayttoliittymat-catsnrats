73a14cb86b9f58a575b2da5903ebf9bcfb152f05
Antti Seppänen <antti.se@gmail.com>
2024-03-19 10:15:40 +0200

    12_keyboard tehtävä

---
+++ 12_Keyboard/12_PaintMouse/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace PaintMouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonSetCoords = new Button();
+            textBoxX = new TextBox();
+            textBoxY = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // buttonSetCoords
+            // 
+            buttonSetCoords.Enabled = false;
+            buttonSetCoords.Location = new Point(149, 37);
+            buttonSetCoords.Name = "buttonSetCoords";
+            buttonSetCoords.Size = new Size(146, 87);
+            buttonSetCoords.TabIndex = 0;
+            buttonSetCoords.Text = "Set \r\ncoordinates";
+            buttonSetCoords.UseVisualStyleBackColor = true;
+            buttonSetCoords.Click += buttonSetCoords_Click;
+            // 
+            // textBoxX
+            // 
+            textBoxX.Enabled = false;
+            textBoxX.Location = new Point(51, 37);
+            textBoxX.Name = "textBoxX";
+            textBoxX.Size = new Size(77, 31);
+            textBoxX.TabIndex = 1;
+            // 
+            // textBoxY
+            // 
+            textBoxY.Enabled = false;
+            textBoxY.Location = new Point(51, 93);
+            textBoxY.Name = "textBoxY";
+            textBoxY.Size = new Size(77, 31);
+            textBoxY.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(27, 25);
+            label1.TabIndex = 3;
+            label1.Text = "X:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 99);
+            label2.Name = "label2";
+            label2.Size = new Size(26, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Y:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 521);
+            label3.Name = "label3";
+            label3.Size = new Size(493, 25);
+            label3.TabIndex = 5;
+            label3.Text = "LIIKUTA UKKOA VAIN NÄPPIKSELLÄ (A, W, S, D) TAI HIIRELLÄ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1024, 555);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBoxY);
+            Controls.Add(textBoxX);
+            Controls.Add(buttonSetCoords);
+            Name = "Form1";
+            Text = "Tikku-ukko";
+            Paint += Form1_Paint;
+            KeyDown += Form1_KeyDown;
+            MouseDown += Form1_MouseDown;
+            MouseMove += Form1_MouseMove;
+            MouseUp += Form1_MouseUp;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonSetCoords;
+        private TextBox textBoxX;
+        private TextBox textBoxY;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ 12_Keyboard/12_PaintMouse/Form1.cs
@@ -0,0 +1,120 @@
+namespace PaintMouse
+{
+    public partial class Form1 : Form
+    {
+        Point piste = new Point(925, 350); // Point objekti             
+        private int nopeus = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics Graf = e.Graphics;
+
+            // piirret‰‰n p‰‰...
+            Graf.FillEllipse(Brushes.MediumAquamarine, piste.X - 4, piste.Y - 8, 49, 49);
+            Graf.DrawEllipse(Pens.Black, piste.X - 4, piste.Y - 8, 49, 49);
+
+            // selk‰
+            Graf.DrawLine(Pens.Black, piste.X + 21, piste.Y + 41, piste.X + 21, piste.Y + 131);
+
+            // k‰det
+            Graf.DrawLine(Pens.Black, piste.X - 30, piste.Y + 60, piste.X + 70, piste.Y + 60);
+
+            // jalat
+            Graf.DrawLine(Pens.Black, piste.X + 21, piste.Y + 131, piste.X - 30, piste.Y + 181);
+            Graf.DrawLine(Pens.Black, piste.X + 21, piste.Y + 131, piste.X + 70, piste.Y + 181);
+
+            // kutsutaan DrawCoordinates()-metodia
+            DrawCordinates(Graf);
+        }
+
+        // DrawCordinates-metodi
+        private void DrawCordinates(Graphics Graf)
+        {
+            // piirt‰‰ piirtokoordinaattien arvot n‰ytˆlle
+            Graf.DrawString("(" + piste.X + ", " + piste.Y + ")", new Font("TimesNewRoman", 14, System.Drawing.FontStyle.Regular), new SolidBrush(Color.Black), 450, 30);
+        }
+
+        private void buttonSetCoords_Click(object sender, EventArgs e)
+        {
+            // parsii X- ja Y-koordinaatit tekstikentist‰
+            if (int.TryParse(textBoxX.Text, out int newX) && int.TryParse(textBoxY.Text, out int newY))
+            {
+                // p‰ivitt‰‰ tikku-ukon koordinaatit
+                piste = new Point(newX, newY);
+
+                // raapustaa formin uudestaan p‰ivitt‰‰kseen tikku-ukon paikan
+                Invalidate();
+            }
+            else
+            {
+                // mik‰li koordinaattien parsiminen ep‰onnistuu
+                MessageBox.Show("Virheelliset koordinaatit. Kokeile uudestaan.", "Virhe", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private bool isDragging = false; // boolean "raahaamista" varten
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // p‰ivitt‰‰ tikku-ukon koordinaatit hiiren painalluksella
+                piste = e.Location;
+
+                // raapustaa formin uudestaan p‰ivitt‰‰kseen tikku-ukon paikan
+                Invalidate();
+            }
+
+            if (e.Button == MouseButtons.Left) // asettaa 'isDraggin' arvoksi true
+            {
+                isDragging = true;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging) // simuloi raahaamisen
+            {
+                // p‰ivitt‰‰ tikku-ukon koordinaatit perustuen hiiren liikkeeseen
+                piste = e.Location;                
+
+                // p‰ivitt‰‰ formin
+                Invalidate();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) // hiiren painikkeen vapauttaminen muuttaa 'isDraggin' arvoon false
+            {
+                isDragging = false;
+            }
+        }
+     
+        private void Form1_KeyDown(object sender, KeyEventArgs e) // w, a, s ja d -n‰pp‰imet tikku-ukon liikuttamiseen
+        {
+            // tarkistukset mit‰ n‰pp‰int‰ on painettu (nopeus-muuttuja apuna koordinaateissa liikkumiseen)
+            if (e.KeyCode == Keys.W)
+            {                
+                piste.Y -= nopeus;               
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                piste.Y += nopeus;
+            }
+            else if (e.KeyCode == Keys.A)
+            {
+                piste.X -= nopeus;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                piste.X += nopeus;
+            }
+
+            Invalidate();
+        }        
+    }
+}
